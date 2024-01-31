6a07524925a2abae35716cba52c7c3ace7aab71e
GoldMane01 <dramos-n@protonmail.com>
2024-01-31 15:34:52 +0100

    Save output into a file

--- pipex/filetest.c
+++ pipex/filetest.c
@@ -1,14 +1,14 @@
 #include "pipex.h"
 
-int main(void)
+void	second_file()
 {
+	int link[2];
+	char *argvec[] = {"wc", "-l", NULL};
+	int pid1;
+	int pid2;
 	char buffer[1025];
 	int bytes;
 	int fd;
-	int link[2];
-	char *argvec[] = {"grep", "ennove", NULL};
-	int pid1;
-	int pid2;
 
 	pipe(link);
 
@@ -17,11 +17,12 @@ int main(void)
 	{
 		close(link[0]);
 		dup2(link[1], STDOUT_FILENO);
-		close(link[0]);
-		fd = open("filexd", O_RDONLY);
+		fd = open("output", O_RDONLY);
 		bytes = read(fd, buffer, 1024);
 		buffer[1024] = '\0';
 		write(STDOUT_FILENO, buffer, bytes);
+		close(link[1]);
+		exit(1);
 	}
 
 	pid2 = fork();
@@ -29,10 +30,53 @@ int main(void)
 	{
 		close(link[1]);
 		dup2(link[0], STDIN_FILENO);
+		fd = open("final", O_RDWR | O_TRUNC);
+		dup2(fd, STDOUT_FILENO);
 		close(link[0]);
-		execve("/bin/grep", argvec, NULL);
+		execve("/bin/wc", argvec, NULL);
 	}
-	close(link[0]);
-	close(link[1]);
+}
+
+void	first_file()
+{
+	int link[2];
+	char *argvec[] = {"ping", "-c", "5", "google.com", NULL};
+	int pid1;
+	int pid2;
+	char buffer[1025];
+	int bytes;
+	int fd;
+
+	pipe(link);
+
+	pid1 = fork();
+	if (pid1 == 0)
+	{
+		close(link[0]);
+		dup2(link[1], STDOUT_FILENO);
+		fd = open("filexd", O_RDONLY);
+		bytes = read(fd, buffer, 1024);
+		buffer[1024] = '\0';
+		write(STDOUT_FILENO, buffer, bytes);
+		close(link[1]);
+		exit(1);
+	}
+
+	pid2 = fork();
+	if (pid2 == 0)
+	{
+		close(link[1]);
+		dup2(link[0], STDIN_FILENO);
+		fd = open("output", O_RDWR);
+		dup2(fd, STDOUT_FILENO);
+		close(link[0]);
+		execve("/bin/ping", argvec, NULL);
+	}
+}
+
+int main(void)
+{
+	first_file();
+	second_file();
 	return 0;
 }
